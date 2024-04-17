87178a05d46b77ca7307fe8cb2d9851c559ae254
ZoraKmiShiro <kd1387463@st.kobedenshi.ac.jp>
2024-04-17 10:30:50 +0900

    テスト
    m_test

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,10 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+	//テスト（後消す）
+	m_test = 5;
+
+
 	return true;
 }
 
